46d77802ead0ea5597f2e6e1b2685ede842a7806
smeenakshi <meenakshi.s@inapp.com>
2024-12-02 16:12:04 +0530

    Text Summarizing

--- MeenakshiDemoApp/MeenakshiDemoApp/Program.cs
+++ MeenakshiDemoApp/MeenakshiDemoApp/Program.cs
@@ -9,6 +9,12 @@ namespace MeenakshiDemoApp
             Types types = new Types();
             types.Array();
             Console.WriteLine("Hello World!");
+
+            var sentence = "This is a very very very very very veryv very very very very very looooong loon loong loooong";
+            var summary = StringUtility.SummerizeText(sentence, 25);
+            Console.WriteLine(summary);
+
         }
     }
+
 }
