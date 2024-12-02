751856340a736904d9323b7de715782af958be05
smeenakshi <meenakshi.s@inapp.com>
2024-12-02 12:35:40 +0530

    New structured Project

---
+++ MeenakshiDemoApp/MeenakshiDemoApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MeenakshiDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Types types = new Types();
+            types.Array();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MeenakshiDemoApp/MeenakshiDemoApp/Types.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeenakshiDemoApp
+{
+    public class Types
+    {
+        public void Array()
+        {
+           string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            Console.WriteLine(cars[0]);
+        }
+    }
+}
